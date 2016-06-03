0a8618ac493b9155e34ae250a0f7c8bc57921dc7
Bryce Arden <arden.bryce@gmail.com>
2016-06-03 10:12:48 -0700

    added tmux resurrect and TPM manager

--- install/tmux.sh
+++ install/tmux.sh
@@ -6,6 +6,10 @@ sudo apt-get install -y python-software-properties software-properties-commont
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
 sudo apt-get install -y tmux=2.0-1~ppa1~t
-
 echo "done installing tmux 2.0, you can check the version be running tmux -V"
+
+echo "installing tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "done installing tmux plugin manager"
 
